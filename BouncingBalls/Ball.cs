6a2d2281c1c2fef4252b2a707e2aa56ca8b1a13b
Alejandro Sánchez González <114105548+Jan0SG@users.noreply.github.com>
2023-02-14 17:17:19 -0600

    Add files via upload

---
+++ BouncingBalls/Ball.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBalls
+{
+    public class Ball
+    {
+        public int size;
+        public int speed;
+        public int direction;
+        private int x;
+        private int y;
+        private Color color;
+        
+       
+        public Ball(int size, int speed, int direction, int x, int y, Color color)//constructor of each ball in the list
+        {
+            this.size = size;
+            this.speed = speed;
+            this.direction = direction;
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            
+           
+        }
+
+        public void Move(int width, int height)//ball movement
+        {
+            double radians = direction * Math.PI / 180;
+            int deltaX = (int)(speed * Math.Cos(radians));
+            int deltaY = (int)(speed * Math.Sin(radians));
+            int newX = x + deltaX;
+            int newY = y + deltaY;
+
+            if (newX < 0 || newX > width - size)
+            {
+                //If the ball hits a horizontal wall, reverse its x direction.
+                direction = 180 - direction;
+                newX = x - deltaX;
+            }
+
+            if (newY < 0 || newY > height - size)
+            {
+                //If the ball hits a vertical wall, reverse its y direction.
+                direction = -direction;
+                newY = y - deltaY;
+            }
+
+            x = newX;
+            y = newY;
+        }
+
+        public void DrawBall(Graphics g)//Draws the ball
+        {
+            Brush brush = new SolidBrush(color);
+            g.FillEllipse(brush, x, y, size, size);
+            
+        }
+
+        public Rectangle Bounds//bounds for collision
+        {
+            get { return new Rectangle(x, y, size, size); }
+        }
+
+        public bool CheckCollision(Ball other)//checks for collision
+        {
+            Rectangle rect1 = new Rectangle(x, y, size, size);
+            Rectangle rect2 = new Rectangle(other.x, other.y, other.size/2, other.size/2);
+            return rect1.IntersectsWith(rect2);
+        }
+
+        private double Distance(int x1, int y1, int x2, int y2)
+        {
+            int xDistance = x2 - x1;
+            int yDistance = y2 - y1;
+            return Math.Sqrt(xDistance * xDistance + yDistance * yDistance);
+        }
+        public bool CollidesWith(Ball otherBall)
+        {
+            double distance = Distance(x + size / 2, y + size / 2, otherBall.x + otherBall.size / 2, otherBall.y + otherBall.size / 2);
+            return distance < size / 2 + otherBall.size / 2;
+        }
+
+        //float distanceBetweenCirclesSquared = (circle2.X - circle1.X) * (circle2.X - circle1.X) + (circle2.Y - circle1.Y) * (circle2.Y - circle1.Y);
+    }
+}
---
+++ BouncingBalls/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBalls
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Graphics g;
+        public Ball ball1, ball2, ball3, ball4, ball5;
+        
+
+
+
+        private Timer timer;
+
+        List<Ball> balls = new List<Ball>();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+
+
+
+            timer = new Timer();
+            timer.Interval = 20;
+            timer.Tick += timer1_Tick_1;
+            timer.Start();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rSize = new Random();//random size
+            var lowerBound1 = 0;
+            var upperBound1 = 130;
+            var rNum1 = rSize.Next(lowerBound1, upperBound1);
+
+            var rSpeed = new Random();// random speed
+            var lowerBound2 = 0;
+            var upperBound2 = 40;
+            var rNum2 = rSpeed.Next(lowerBound2, upperBound2);
+
+            var rDirection = new Random();// random direction
+            var lowerBound3 = 0;
+            var upperBound3 = 360;
+            var rNum3 = rDirection.Next(lowerBound3, upperBound3);
+
+            var random4 = new Random(); // random color
+            Color randomColor = Color.FromArgb(random4.Next(256), random4.Next(256), random4.Next(256));
+
+
+            balls.Add(new Ball(rNum1, rNum2, rNum3, 10, 10, randomColor));
+
+
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+
+            var rDirec = new Random();
+            var lowerBound = 0;
+            var upperBound = 360;
+            var rD = rDirec.Next(lowerBound, upperBound);
+
+
+            for (int i = 0; i < balls.Count; i++)// ball is drawn and moves based on the number of elements in the list
+            {
+
+                balls[i].DrawBall(g);
+                balls[i].Move(pictureBox1.Width, pictureBox1.Height);
+
+                for(int j = i + 1; j < balls.Count; j++)//Start of collision
+                {
+
+                    if (balls[i].Bounds.IntersectsWith(balls[j].Bounds))
+                    {
+                        double distance = Math.Sqrt(Math.Pow(balls[i].Bounds.X - balls[j].Bounds.X, 2) + Math.Pow(balls[i].Bounds.Y - balls[j].Bounds.Y, 2));
+
+                        if (distance <= balls[i].Bounds.Width / 2 + balls[j].Bounds.Width / 2)
+                        {
+
+                            //collision is ocurring
+                            balls[i].direction = rD - balls[i].direction;
+                            balls[j].direction = rD - balls[j].direction;
+                        }
+
+
+                    }
+
+
+                }//End of collision
+            }
+
+            
+        }
+
+    }
+}
